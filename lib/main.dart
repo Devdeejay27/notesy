ec9be4a0415599b9e682b5ae4758fbc3a2e374b9
Dumeje <dum_obieli@yahoo.com>
2024-10-30 18:33:06 +0100

    step-4

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ import 'package:notesy/firebase_options.dart';
 import 'package:notesy/views/login_view.dart';
 import 'package:notesy/views/register_view.dart';
 import 'package:notesy/views/verify_email_view.dart';
-import 'dart:developer' as devtools show log;
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -20,6 +19,7 @@ void main() {
       routes: {
         '/login/': (context) => const LoginView(),
         '/register/': (context) => const RegisterView(),
+        '/notes/': (context) => const NotesView(),
       },
     ),
   );
