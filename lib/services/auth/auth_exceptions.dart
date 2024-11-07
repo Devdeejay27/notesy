814ce44d5fbfd12bac4070716ce59b4b64cd786f
Dumeje <dum_obieli@yahoo.com>
2024-11-07 16:01:04 +0100

    step-8

---
+++ lib/services/auth/auth_exceptions.dart
@@ -0,0 +1,11 @@
+// login exceptions
+
+class InvalidEmailOrPasswordAuthException implements Exception {}
+
+// register exceptions
+
+class WeakPasswordAuthException implements Exception {}
+
+class EmailAlreadyInUseAuthException implements Exception {}
+
+class InvalidEmailAuthException implements Exception {}
