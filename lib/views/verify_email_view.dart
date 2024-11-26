180d697cb1427d29c768dc9613986c24fe33dd5c
Dumeje <dum_obieli@yahoo.com>
2024-11-26 10:56:36 +0100

    step-25

--- lib/views/verify_email_view.dart
+++ lib/views/verify_email_view.dart
@@ -1,8 +1,7 @@
-// ignore_for_file: use_build_context_synchronously
-
 import 'package:flutter/material.dart';
-import 'package:notesy/constants/routes.dart';
-import 'package:notesy/services/auth/auth_service.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notesy/services/auth/bloc/auth_bloc.dart';
+import 'package:notesy/services/auth/bloc/auth_event.dart';
 
 class VerifyEmailView extends StatefulWidget {
   const VerifyEmailView({super.key});
@@ -24,18 +23,18 @@ class _VerifyEmailViewState extends State<VerifyEmailView> {
           const Text(
               'If you have not received a verification email, press the button below'),
           TextButton(
-            onPressed: () async {
-              await AuthService.firebase().sendEmailVerification();
+            onPressed: () {
+              context.read<AuthBloc>().add(
+                    const AuthEventSendEmailVerification(),
+                  );
             },
             child: const Text('Send email verification'),
           ),
           TextButton(
-            onPressed: () async {
-              await AuthService.firebase().logOut();
-              Navigator.of(context).pushNamedAndRemoveUntil(
-                registerRoute,
-                (route) => false,
-              );
+            onPressed: () {
+              context.read<AuthBloc>().add(
+                    const AuthEventLogOut(),
+                  );
             },
             child: const Text('Restart'),
           ),
